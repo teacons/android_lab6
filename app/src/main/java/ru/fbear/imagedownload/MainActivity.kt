3e4e18ae2fc630b72f8bbc22c40aca81dc18ccdf
teacons <teacons@fbear.ru>
2020-12-08 01:43:12 +0300

    Glide

--- app/src/main/java/ru/fbear/imagedownload/MainActivity.kt
+++ app/src/main/java/ru/fbear/imagedownload/MainActivity.kt
@@ -1,41 +1,23 @@
 package ru.fbear.imagedownload
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.coroutineScope
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         download_btn.setOnClickListener {
-            val urlText = if (url.text.isNotEmpty()) {
-                url.text.toString()
-            } else {
-                "https://old.fbear.ru/image/android_cat.webp"
-            }
-            lifecycle.coroutineScope.launchWhenResumed {
-                image.setImageBitmap(downloadImage(urlText))
-            }
+            val urlText =
+                if (url.text.isNotEmpty()) {
+                    url.text.toString()
+                } else {
+                    "https://old.fbear.ru/image/android_cat.webp"
+                }
+            Glide.with(this).load(urlText).into(image)
         }
     }
-
-    private suspend fun downloadImage(url: String): Bitmap? =
-        withContext(Dispatchers.IO) {
-            try {
-                BitmapFactory.decodeStream(URL(url).openStream())
-            } catch (e: Exception) {
-                e.message?.let { Log.e("Error", it) }
-                e.printStackTrace()
-                null
-            }
-        }
 }
 
